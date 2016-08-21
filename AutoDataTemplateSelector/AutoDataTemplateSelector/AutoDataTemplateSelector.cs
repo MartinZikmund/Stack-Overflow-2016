4ea4456a706f444430cf45cd46284cee07d63b34
Martin Zikmund <martinzikmund@sphereline.com>
2016-08-21 11:04:04 +0200

    AutoDataTemplateSelector
    http://stackoverflow.com/questions/39055964/how-can-i-set-datatemplate-to-a-contentcontrol-in-a-pageresource-in-windows-univ

---
+++ AutoDataTemplateSelector/AutoDataTemplateSelector/AutoDataTemplateSelector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace AutoDataTemplateSelector
+{
+    public class AutoDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore( object item ) => GetTemplateForItem( item, null );
+
+        protected override DataTemplate SelectTemplateCore( object item, DependencyObject container ) => GetTemplateForItem( item, container );
+
+        private DataTemplate GetTemplateForItem( object item, DependencyObject container )
+        {
+            if ( item != null )
+            {
+                var viewModelTypeName = item.GetType().Name;
+                var dataTemplateInTree = FindResourceKeyUpTree( viewModelTypeName, container );
+                //return or default to Application resource
+                return dataTemplateInTree ?? ( DataTemplate )Application.Current.Resources[ viewModelTypeName ];
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Tries to find the resources up the tree
+        /// </summary>
+        /// <param name="resourceKey">Key to find</param>
+        /// <param name="container">Current container</param>
+        /// <returns></returns>
+        private DataTemplate FindResourceKeyUpTree( string resourceKey, DependencyObject container )
+        {
+            var frameworkElement = container as FrameworkElement;
+            if ( frameworkElement != null )
+            {
+                if ( frameworkElement.Resources.ContainsKey( resourceKey ) )
+                {
+                    return frameworkElement.Resources[ resourceKey ] as DataTemplate;
+                }
+                else
+                {
+                    return FindResourceKeyUpTree( resourceKey, VisualTreeHelper.GetParent( frameworkElement ) );
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ AutoDataTemplateSelector/AutoDataTemplateSelector/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AutoDataTemplateSelector
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SwitchClick( object sender, RoutedEventArgs e )
+        {
+            if (CurrentViewModel is RedViewModel)
+            {
+                CurrentViewModel = new BlueViewModel();                
+            }
+            else
+            {
+                CurrentViewModel = new RedViewModel();
+            }
+        }
+
+        private ViewModel _currentViewModel = new RedViewModel();
+        public ViewModel CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged( [CallerMemberName] string propertyName = null )
+        {
+            PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+        }
+    }
+}
