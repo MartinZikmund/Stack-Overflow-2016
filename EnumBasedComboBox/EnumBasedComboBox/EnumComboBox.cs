1d9a105cf114e94cf032772c4bddeaa158f78d0a
Martin Zikmund <martinzikmund@sphereline.com>
2016-09-02 20:58:27 +0200

    EnumBasedComboBox - http://stackoverflow.com/questions/39296222/uwp-subclassed-combobox-control-in-c-sharp-for-enum-display

---
+++ EnumBasedComboBox/EnumBasedComboBox/EnumComboBox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace EnumBasedComboBox
+{
+    public class EnumComboBox : ComboBox
+    {
+        public static readonly DependencyProperty SelectedItemEnumProperty = DependencyProperty.Register(
+            "SelectedItemEnum", typeof( object ), typeof( EnumComboBox ), new PropertyMetadata( default( object ), SelectedItemEnumChanged ) );
+
+        private static void SelectedItemEnumChanged( DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e )
+        {
+            if ( e.NewValue != null )
+            {
+                if ( e.OldValue == null || e.OldValue.GetType() != e.NewValue.GetType() )
+                {
+                    var enumComboBox = ( EnumComboBox )dependencyObject;
+                    var values = Enum.GetValues( enumComboBox.SelectedItemEnum.GetType() ).Cast<object>().ToList();
+                    enumComboBox.ItemsSource = values;
+                }
+            }
+        }
+
+        public object SelectedItemEnum
+        {
+            get { return ( object )GetValue( SelectedItemEnumProperty ); }
+            set { SetValue( SelectedItemEnumProperty, value ); }
+        }
+    }
+}
---
+++ EnumBasedComboBox/EnumBasedComboBox/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using TestComponent;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EnumBasedComboBox
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            FontStyleContent.Content = FontStyle.Normal;
+            CustomContent.Content = Test.A;
+            ComponentEnumContent.Content = ApplicationDataLocality.Local;            
+        }
+
+        private void FontStyleClick(object sender, RoutedEventArgs e)
+        {
+            EnumComboBox.SelectedItemEnum = FontStyle.Normal;
+        }
+
+        private void CustomClick( object sender, RoutedEventArgs e )
+        {
+            EnumComboBox.SelectedItemEnum = Test.A;
+        }
+
+        private void ComponentEnumClick(object sender, RoutedEventArgs e)
+        {
+            EnumComboBox.SelectedItemEnum = ComponentEnum.A;
+        }
+    }
+
+    public enum Test
+    {
+        A, B, C
+    }
+}
