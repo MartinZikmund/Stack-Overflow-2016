d4d03f4496a0f8e79f8ff7437f5ac68f5dc74831
Martin Zikmund <martinzikmund@sphereline.com>
2016-09-02 09:24:57 +0200

    Late-load FlipView (http://stackoverflow.com/questions/39277174/load-only-selected-item-in-a-xaml-flipview)

---
+++ LateLoadFlipView/LateLoadFlipView/FlipViewItemViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace LateLoadFlipView
+{
+    public class FlipViewItemViewModel : INotifyPropertyChanged
+    {
+        private bool _isLoaded = false;
+
+        private string _dateLoaded = "";
+
+        public string DateLoaded
+        {
+            get
+            {
+                return _dateLoaded;
+            }
+            set
+            {
+                _dateLoaded = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ImageSource _imageSource = null;
+
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return _imageSource;
+            }
+            set
+            {
+                _imageSource = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Forces the loading of the item
+        /// </summary>
+        public void ForceLoad()
+        {
+            //prevent loading twice
+            if ( !_isLoaded )
+            {                
+                //set current date (just to prove it didn't load aheead of time)
+                DateLoaded = DateTime.Now.ToString( "F" );
+                //load the image (probably from network?)
+                ImageSource = new BitmapImage( new Uri( "ms-appx:///Assets/StoreLogo.png" ) );
+
+                _isLoaded = true;
+            }
+        }
+
+        /// <summary>
+        /// INotifyPropertyChanged implementation
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged( [CallerMemberName] string propertyName = null )
+        {
+            PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+        }
+    }
+}
---
+++ LateLoadFlipView/LateLoadFlipView/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LateLoadFlipView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Just a sample data source
+        /// </summary>
+        public ObservableCollection<FlipViewItemViewModel> Items = new ObservableCollection<FlipViewItemViewModel>()
+        {
+            new FlipViewItemViewModel(),
+            new FlipViewItemViewModel(),
+            new FlipViewItemViewModel(),
+            new FlipViewItemViewModel(),
+            new FlipViewItemViewModel(),
+            new FlipViewItemViewModel(),
+            new FlipViewItemViewModel()
+        };
+
+        private void Selector_OnSelectionChanged( object sender, SelectionChangedEventArgs e )
+        {
+            //get the currently selected item
+            var currentItem = FlipControl.SelectedItem as FlipViewItemViewModel;
+            //force-load it
+            currentItem?.ForceLoad();
+        }
+    }
+}
