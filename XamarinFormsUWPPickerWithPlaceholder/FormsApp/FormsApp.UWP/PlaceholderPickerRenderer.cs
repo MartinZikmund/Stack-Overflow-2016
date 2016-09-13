f2ec26007c321d010abdb2ef6c9c99155ac8e140
Martin Zikmund <martinzikmund@sphereline.com>
2016-09-13 10:52:04 +0200

    Xamarin Forms Placeholder Picker - http://stackoverflow.com/questions/39444533/picker-text-goes-out-of-picker-box/39449420

---
+++ XamarinFormsUWPPickerWithPlaceholder/FormsApp/FormsApp.UWP/PlaceholderPickerRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormsApp;
+using FormsApp.UWP;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+[assembly: ExportRenderer( typeof( PlaceholderPicker ), typeof( PlaceholderPickerRenderer ) )]
+namespace FormsApp.UWP
+{
+    
+
+    public class PlaceholderPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {            
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.PlaceholderText = Element.Title;
+                Control.Header = null;
+            }
+        }
+    }
+}
