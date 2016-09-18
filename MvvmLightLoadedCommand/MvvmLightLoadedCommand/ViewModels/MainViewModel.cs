04caa1f03f716d68eef458bc93d3e2d503a4607b
Martin Zikmund <martinzikmund@sphereline.com>
2016-09-18 11:21:19 +0200

    MvvmLight Loaded Command - http://stackoverflow.com/questions/39549793/mvvm-light-uwp-bind-event-to-command-and-get-refference-to-ui-element

---
+++ MvvmLightLoadedCommand/MvvmLightLoadedCommand/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Popups;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace MvvmLightLoadedCommand.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {      
+        public ICommand LoadedCommand { get; } = new RelayCommand( async () => await new MessageDialog( "Loaded", "Some Content" ).ShowAsync() );
+    }
+}
---
+++ MvvmLightLoadedCommand/MvvmLightLoadedCommand/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using Microsoft.Practices.ServiceLocation;
+
+namespace MvvmLightLoadedCommand.ViewModels
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider( () => SimpleIoc.Default );
+           
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes." )]
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+    }
+}
